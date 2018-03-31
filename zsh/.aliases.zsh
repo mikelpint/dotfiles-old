bad46f329704914f10dc46b12cf62eae7595489e
Mikel Pintado <mikelaitornube2010@gmail.com>
2018-04-01 00:15:35 +0200

    Adds and modifies a lot of aliases and modifies other Zsh settings.

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -2,9 +2,6 @@
 alias df='df -h'
 alias du='du -h -d 2'
 
-# List files matching the specified name
-alias lsg='ll | grep'
-
 # PS
 alias psa="ps aux"
 alias psg="ps aux | grep "
@@ -27,6 +24,14 @@ if [ -f /usr/bin/nvim ]; then
     alias vim='nvim'
 fi
 
+# Replace ls with Exa
+if [ -f /usr/bin/exa ]; then
+    alias ls='exa'
+fi
+
+# List files matching the specified name
+alias lsg='ls -l | grep'
+
 # Editing dotfiles
 alias zshconf="$EDITOR ~/.zshrc"
 alias visudo="$EDITOR /etc/sudoers"
@@ -45,6 +50,7 @@ alias rm='nocorrect rm'
 alias tman='/usr/bin/man' # Man
 alias tvim='/usr/bin/vim' # Vim
 alias tcat='/usr/bin/cat' # Cat
+alias tls='/usr/bin/ls' # LS
 
 # Make sudo don't ignore aliases
 alias sudo='nocorrect sudo'
@@ -60,18 +66,16 @@ alias gsh='git show'
 alias gshw='git show'
 alias gshow='git show'
 alias gi='vim .gitignore'
-alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
-alias gco='git co'
-alias gcp='git cp'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gcp='git cherry-pick -x'
 alias ga='git add -A'
 alias gap='git add -p'
 alias guns='git unstage'
 alias gunc='git uncommit'
 alias gm='git merge'
 alias gms='git merge --squash'
-alias gam='git amend --reset-author'
+alias gam='git commit --amend --reset-author'
 alias grv='git remote -v'
 alias grr='git remote rm'
 alias grad='git remote add'
@@ -79,18 +83,14 @@ alias gr='git rebase'
 alias gra='git rebase --abort'
 alias ggrc='git rebase --continue'
 alias gbi='git rebase --interactive'
-alias gl='git l'
-alias glg='git l'
-alias glog='git l'
+alias gl='git log'
 alias glc='git log --graph --decorate --oneline --all'
-alias co='git co'
 alias gf='git fetch'
 alias gfp='git fetch --prune'
 alias gfa='git fetch --all'
 alias gfap='git fetch --all --prune'
-alias gfch='git fetch'
 alias gd='git diff'
-alias gb='git b'
+alias gb='git branch --verbose'
 alias gdc='git diff --cached -w'
 alias gds='git diff --staged -w'
 alias gpub='grb publish'
@@ -99,7 +99,7 @@ alias gpl='git pull'
 alias gplr='git pull --rebase'
 alias gps='git push'
 alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias gnb='git nb'
+alias gnb='git checkout -b'
 alias grs='git reset'
 alias grsh='git reset --hard'
 alias gcln='git clean'
@@ -108,7 +108,7 @@ alias gclndfx='git clean -dfx'
 alias gsm='git submodule'
 alias gsmi='git submodule init'
 alias gsmu='git submodule update'
-alias gt='git t'
+alias gt='git tag -n'
 alias gbg='git bisect good'
 alias gbb='git bisect bad'
 alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,16 +1,16 @@
 # Set editor to Vim
-export VISUAL=/usr/bin/vim
+export VISUAL=nvim
 export EDITOR=$VISUAL
 
 # Add Gem packages to $PATH
-export PATH=$PATH:~/.gem/ruby/2.4.0/bin
+export PATH=$PATH:~/.gem/ruby/2.5.0/bin
 
 # Set language to Spanish.
 export LANG=es_ES.UTF-8
 
 # Tmux plugin's configuration
 export ZSH_TMUX_AUTOSTART=true
-export ZSH_TMUX_AUTOCONNECT=false
+export ZSH_TMUX_AUTOCONNECT=true
 
 # Theme
 ZSH_THEME="ys"
